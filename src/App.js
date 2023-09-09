66328901b907ea48de377d2aa7b8ddf30c8da19c
rayen <rayen.andolsi98@gmail.com>
2023-09-09 07:35:56 +0100

    change to react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const birthDate = new Date("06 06 2024 00:00:00").getTime();
+  const [msg, setMsg] = useState(false);
+  const [days, setDays] = useState();
+  const [hours, setHours] = useState();
+  const [minutes, setMinutes] = useState();
+  const [seconds, setSeconds] = useState();
+
+  let countDown = setInterval(() => {
+    let nowDate = new Date().getTime();
+    let dateDiff = birthDate - nowDate;
+
+    setDays(Math.floor(dateDiff / (1000 * 60 * 60 * 24)));
+    setHours(Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    setMinutes(Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60)));
+    setSeconds(Math.floor((dateDiff % (1000 * 60)) / 1000));
+    
+    if (dateDiff < 0) {
+      clearInterval(countDown);
+      setMsg(true);
+    }
+  }, 1000);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {msg ? (
+        <p className="msg">{process.env.REACT_APP_THE_MESSAGE}</p>
+      ) : (
+        <>
+          <h2 className="title">Happy Birthday To Me</h2>
+          <div className="box">
+            <span id="days" data-name="days">
+              {days || 0}
+            </span>
+            <span id="hours" data-name="hours">
+              {hours || 0}
+            </span>
+            <span id="minutes" data-name="minutes">
+              {minutes || 0}
+            </span>
+            <span id="seconds" data-name="secends">
+              {seconds || 0}
+            </span>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
